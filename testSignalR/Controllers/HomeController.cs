6335325b61c680a62af00db8de34114f027d0170
waiholiu <waiholiuEmail>
2017-11-07 16:23:49 +1100

    test change

--- testSignalR/Controllers/HomeController.cs
+++ testSignalR/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace testSignalR.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            //change
             return View();
         }
     }
---
+++ testSignalR/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.SignalR;
+
+namespace testSignalR
+{
+    public class MyHub : Hub
+    {
+        public void Announce(string message)
+        {
+            Clients.All.Announce(message + " changed");
+        }
+
+    }
+}
